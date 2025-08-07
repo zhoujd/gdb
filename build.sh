98d89f79615ac2c14ed92cc3577cad914e2194ad
Zachary Zhou <zchrzhou@gmail.com>
2025-08-07 10:25:19 +0800

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+SRC_ROOT=$SCRIPT_ROOT/src
+BUILD_ROOT=$SRC_ROOT/build
+
+. /etc/os-release
+
+GDB_VER=11.2
+GDB_SRC=gdb-${GDB_VER}
+GDB_PREFIX=/opt/zach/${GDB_SRC}
+GDB_OPT=(
+    --prefix=${GDB_PREFIX}
+    --with-python=/usr/bin/python3
+)
+
+install_dep() {
+    case $ID in
+        ubuntu|debian )
+            echo "Install on $ID"
+            sudo apt update
+            sudo apt install -y build-essential texinfo bison flex
+            ;;
+        * )
+            echo "Unsupport $ID"
+            ;;
+    esac
+    echo "Install build deps done"
+}
+
+install_bin() {
+    target=/usr/local/bin
+    echo "Install gdb bin to $target"
+    sudo ln -sfvT $GDB_PREFIX/bin/gdb $target/gdb
+    echo "Install bin done"
+}
+
+build() {
+    echo "Build run autogen.sh"
+    echo "Build ${GDB_SRC}"
+    mkdir -p $BUILD_ROOT
+    pushd $BUILD_ROOT
+    $SRC_ROOT/configure ${GDB_OPT[@]}
+    make -j8
+    sudo make install
+    install_bin
+    popd
+    echo "Build done"
+}
+
+clean() {
+    pushd $SCRIPT_ROOT
+    git clean -dfx
+    popd
+    echo "Clean done"
+}
+
+usage() {
+    app=$(basename $0)
+    cat <<EOF
+Usage: $app {dep|-d|-t|build|-b|bin|clean|-c|all|-a}
+EOF
+}
+
+case $1 in
+    dep|-d )
+        install_dep
+        ;;
+    build|-b )
+        build
+        ;;
+    bin )
+        install_bin
+        ;;
+    clean|-c )
+        clean
+        ;;
+    all|-a )
+        install_dep
+        build
+        clean
+        ;;
+    * )
+        usage
+        ;;
+esac
